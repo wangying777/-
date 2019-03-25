620efaacdf5cdd19c78833f61a3a7190eff52141
wangying777 <3081586151@qq.com>
2019-03-25 09:34:27 +0800

    eee

---
+++ 移动端/练习/index.js
@@ -0,0 +1,42 @@
+// var iNow =1;
+// $(document).on('swipeUp',function(){
+//     $(".page-"+iNow).attr("class","page-"+iNow+" moveToTop");
+//     $(".page-"+(iNow+1)).attr("class","page-"+(iNow+1)+" moveFromBottom");
+//     setTimeout(function(){
+//         $(".page-"+(iNow+1)).children().removeClass('hide');
+//         $(".page-"+iNow).children().addClass('hide');
+//         iNow++;
+//     },600);
+// })
+
+var iNow = 1;
+$(document).on('swipeUp',function(){
+    // console.log(1111111);
+    // 当前图片 滑上去   添加class "page-1 moveToTop"
+    $('.page-'+iNow).attr("class","page-"+iNow+" moveToTop");
+    // 找到下一张图 从下边滑出来   "page-2 moveFromBottom"(iNow+1)
+    $('.page-'+(iNow+1)).attr("class","page-"+(iNow+1)+" moveFromBottom"  );
+
+    // 0.6s后 设置图片动画显示 
+    setTimeout(function(){
+        $('.page-'+(iNow+1)).children().removeClass('hide');
+        // 滑出的中的小图片 隐藏
+        $('.page-'+iNow).children().addClass('hide');
+        iNow++;
+    }, 600);
+});
+
+$(document).on('swipeDown',function(){
+    // 当前图片 滑下 
+    $('.page-'+iNow).attr('class','page-'+iNow+" moveToBottom");
+    // 上一张图 滑下来 
+    $('.page-'+(iNow-1)).attr('class','page-'+(iNow-1)+" moveFromTop");
+
+    setTimeout(function(){
+        $('.page-'+(iNow-1)).children().removeClass('hide');
+        // 下滑的时候 让出去的小图都隐藏
+        $('.page-'+iNow).children().addClass('hide');
+        iNow--;
+    }, 600);
+
+});
